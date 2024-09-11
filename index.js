c1890198ba026e382d18fd8270d7ce026e4bc258
Bianor Araujo <bianor.araujo@gmail.com>
2024-09-10 23:43:44 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express")
+const app = express()
+
+const mongoose = require("mongoose");
+const userService = require("./services/userService");
+//const config = require("./config/index");
+
+mongoose.connect("mongodb+srv://cardAdmin:testeMongoDb@cluster0.nyqam.mongodb.net/card?retryWrites=true&w=majority&appName=Cluster0");
+
+app.get("/", (req, res) => {
+    res.send("Hello Word!")
+})
+
+app.post("/add", async (req, res) => {
+    var status = await userService.Create("bianor.araujo@gmail.com", "123456")
+
+        if(status){
+            res.send("Added!");
+            res.redirect("/");
+        }
+        else{
+            res.send("Ocorreu uma falha!");
+        }
+})
+
+// app.get("/getUser", async (req, res) => {
+//     var user = await userService.GetAll(false);
+
+//     res.json(appointment);
+// })
+
+
+
+app.listen(3002, () => {
+    console.log(`Linten on port 3002...`);
+});
